5da0c7397e2ffed558c1cc606fe9866c43897464
maylopezga <maye.lopez16@gmail.com>
2017-04-20 17:58:08 -0500

    Herramientas frecuentes

--- app/controllers/lends_controller.rb
+++ app/controllers/lends_controller.rb
@@ -1,16 +1,61 @@
 class LendsController < ApplicationController
-  def index
-  end
 
     before_action :get_tools
     before_action :get_students
 
+      def tag
+        #ban = true
+        puts "entro"
+
+        port_str = "/dev/ttyACM0"  #may be different for you
+        baud_rate = 115200
+        data_bits = 8
+        stop_bits = 1
+        parity = SerialPort::NONE
+
+        sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+      #  while ban do
+          while (i = sp.gets.chomp) do
+            i.slice!("Tag is not NDEF formatted.")
+            tagUID = i
+            puts "el while"
+            puts tagUID
+            if tagUID.empty?
+               ban= true
+            else
+              return tagUID
+            end
+
+
+
+          end
+        #end
+      #sp.closes
+      end
+
+
     def index
+      @frecuentes = ["Kit electrónica", "Cautín", "Pinza", "Pela cable", "Corta frío", "Multímetro",
+                      "Kit lego", "Destornillador", "Fuente", "Flexómetro", "Hexágonas", "Pie de rey"]
+      @lends = Lend.all
       @lends = Lend.new
+      #@lends.uid = tag
+      #@lends = Lend.search(params[:search])
     end
     def create
-      @lends = Lend.new(student: params[:lend])
-      @lends.students = params[:students]
+    #  @students = @students.where("name = ?", "%#{params[:uid]}%")
+      @students = @students.find_by(taguid: :uid)
+      puts "----------estudiante -----------"
+      #@students.each do |student|
+      #  puts "Es #{student}"
+      #end
+      puts @students
+      puts "-----------fin -------------"
+      @lends = Lend.new(lend_params)
+      #@lends.students = params[:students]
+      #@lends.each do |p|
+      #  puts p.uid
+      #end
       @lends.save
       redirect_to control_url
     end
@@ -19,7 +64,7 @@ class LendsController < ApplicationController
 
     private
     def lend_params
-      params.require(:lend).permit(:iud)
+      params.require(:lend).permit(:uid, :student_id, :tool_id)
     end
     def get_tools
       @tools = Tool.all
@@ -29,7 +74,7 @@ class LendsController < ApplicationController
     end
 
     def show
-      @tools = @tools.where("title ILIKE ?", "%#{params[:title]}%")
+      @students = @students.where("uid ILIKE ?", "%#{params[:taguid]}%")
       #@pro = @tools.title
     end
 
--- app/models/lend.rb
+++ app/models/lend.rb
@@ -1,4 +1,12 @@
 class Lend < ApplicationRecord
   belongs_to :tool
   belongs_to :student
+
+  def self.search(search)
+    if search
+      where("title LIKE ?", "%#{search}%")
+    else
+      all
+    end
+  end
 end
