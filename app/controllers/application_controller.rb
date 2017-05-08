46df87ba44c448e28fdc87a9ead31e96d85f6b2b
maylopezga <maye.lopez16@gmail.com>
2017-05-08 15:37:14 -0500

    Ya registra estudiantes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :set_tools
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+
+   protected
+
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :cel, :code])
+   end
 
   private
   def set_tools
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-
-  devise_for :students
+  devise_for :students#, :controllers => { :registrations => "students/resgistration"}
   get "tools", to: "tools#index"
 
   get "control", to: "lends#index"
   resources :lends
-  root 'students#index'
-  resources :students
+  #root 'students#index'
+  #resources :students
+  root  'tools#index'
   resources :tools
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
