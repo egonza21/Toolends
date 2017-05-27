3af28bfdd489079d82261b09a236dda39d7fe897
egonza21 <egonza21@outlook.com>
2017-05-26 19:18:54 -0500

    Error de Mendoza

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+// alert('Hola mundo');
+
+// $('#select').click(function() {
+//   console.log("Entro");
+//   alert('select tool');
+// });
